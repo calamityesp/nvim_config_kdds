cda357a8c28a8f7792f925d29499c982c4b08416
calamityesp <calamity_xemus@hotmail.com>
2025-06-16 20:36:50 -0600

    initial commit

---
+++ lua/kdds/config/init.lua
@@ -0,0 +1,7 @@
+-- Set to true if you have a Nerd Font installed
+vim.g.have_nerd_font = false
+
+require("kdds.config.autocmd")
+require("kdds.config.remap")
+require("kdds.config.settings")
+require("kdds.config.options")
