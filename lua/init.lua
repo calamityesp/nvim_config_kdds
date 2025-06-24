0b26bbb958cc631d49d0009d87cdb918db66074d
calamityesp <calamity_xemus@hotmail.com>
2025-06-23 20:49:32 -0600

    redoing my config

--- lua/init.lua
+++ lua/init.lua
@@ -1,4 +1,29 @@
-return {
-   "nvim-lua/plenary.nvim",
-   "christoomey/vim-tmux-navigator",
-}
+-- mapping the leader key 
+vim.g.mapleader = " "
+vim.g.maplocalleader = ' '
+
+require("config")
+require("utils")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({ { import = "plugins" }, {
+  checker = {
+    enabled = true,
+    notify = false
+  },
+  change_detection = {
+    notify = false
+  },
+})
