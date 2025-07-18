abdce61b1a1518ed48462e81e2179cb25abd0ef7
calamityesp <calamity_xemus@hotmail.com>
2025-07-17 23:35:30 -0600

    adding some plugins, some configs, and stuff

---
+++ lua/plugins/images.lua
@@ -0,0 +1,11 @@
+return {
+    -- "3rd/image.nvim",
+    -- dependencies = { "luarocks.nvim" },
+    -- config = function()
+    --     require("image").setup({
+    --         max_height_window_percentage = 50,
+    --         processor = "magick_cli",
+    --         hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
+    --     })
+    -- end,
+}
