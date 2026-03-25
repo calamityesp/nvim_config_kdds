89129738e93f6b086c1e4fe6dbeed99fc64c27e7
Calamity <calamity_xemus@hotmail.com>
2026-03-25 09:33:39 -0600

    removing builtin picker

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -57,7 +57,7 @@ return {
                 time_format = "%H:%M",
             },
             picker = {
-                name = "snacks.nvim",
+                name = nil,
                 note_mappings = {
                     new = "<C-x>",
                     insert_link = "<C-l>",
